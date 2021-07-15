496b6382e19df1779e11f8d1e6a62184cf3acf9b
Viren <virenpatil1@outlook.com>
2021-07-15 22:49:56 +0200

    Created the sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,45 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
+  const recentItem = (topic) => {
+    return (
+      <div className='sidebar__recentItem'>
+        <span className='sidebar__hash'>#</span>
+        <p>{topic}</p>
+      </div>
+    );
+  };
+
   return (
     <div className='sidebar'>
-      <h1>Sidebar</h1>
+      <div className='sidebar__top'>
+        <img src='https://miro.medium.com/max/1400/0*9HT-cCMaBZZZtbi8' alt='' />
+        <Avatar
+          className='side__avatar'
+          src='https://i.ibb.co/ZdzdX7L/IMG-8949.jpg'
+        />
+        <h2>Viren Patil </h2>
+        <h4>virenpatil1@outlook.com</h4>
+      </div>
+      <div className='sidebar__stats'>
+        <div className='sidebar__stat'>
+          <p>Who viewed you</p>
+          <p className='sidebar__statNumber'>101</p>
+        </div>
+        <div className='sidebar__stat'>
+          <p>Views on post</p>
+          <p className='sidebar__statNumber'>2443</p>
+        </div>
+      </div>
+      <div className='sidebar__bottom'>
+        <p>Recent</p>
+        {recentItem('Reactjs')}
+        {recentItem('Javascript')}
+        {recentItem('Frontend')}
+        {recentItem('Desgin')}
+      </div>
     </div>
   );
 }
